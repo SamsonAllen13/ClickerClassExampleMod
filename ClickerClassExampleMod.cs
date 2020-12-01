290e65f16137ad5ef26699b8fe8dfda256c7fea4
direwolf420 <amakejk1@web.de>
2020-12-01 20:40:33 +0100

    remove a todo

--- ClickerClassExampleMod.cs
+++ ClickerClassExampleMod.cs
@@ -2,8 +2,6 @@ using Terraria.ModLoader;
 
 namespace ClickerClassExampleMod
 {
-	//TODO for developers:
-	//Add some projectile example
 	public class ClickerClassExampleMod : Mod
 	{
 		public override void Load()
